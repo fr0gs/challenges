1f8ac4c524a41fa18bdd756db6333177af03c1e9
fr0gs <fr0gs@bitbucket.org>
2015-09-27 19:10:51 +0200

    Multiple 3 & 5 Javascript

---
+++ mul_3_5/mul.js
@@ -0,0 +1,15 @@
+
+var sumFunc = function() {
+	var sum = 0;
+
+	for (var i=0; i<1000; i++){
+		if ((i % 3 == 0) || (i % 5 == 0)) {
+			sum += i;
+		}
+	} 
+	return sum;
+}
+
+
+console.log(sumFunc());
+
