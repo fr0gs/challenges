cf314298668d4ece2e74a5b17a6bc19a90126b2c
fr0gs <elf_elegua@yahoo.com>
2015-11-13 12:20:55 +0100

    Final logic commit. TODO: style the calculator

--- FreeCodeCamp/ember-calculator/app/models/calculator.js
+++ FreeCodeCamp/ember-calculator/app/models/calculator.js
@@ -3,19 +3,21 @@
   I(we) should only include (computed) properties and data. From here one could
   retrieve data from a DB or a REST api I guess without breaking the design pattern.
 */
+import Ember from 'ember';
+
 var Calculator = Ember.Object.extend({
   operation: {
     '+': function(op1, op2) {
-      return (op1 + op2)
+      return (op1 + op2);
     },
     '-': function (op1, op2) {
-      return (op1 - op2)
+      return (op1 - op2);
     },
     'X': function (op1, op2) {
-      return (op1 * op2)
+      return (op1 * op2);
     },
     '%': function (op1, op2) {
-      return (op1 / op2)
+      return (op1 / op2);
     }
   },
 
