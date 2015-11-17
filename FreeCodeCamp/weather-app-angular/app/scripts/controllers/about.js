084f0cb1e2ee3178b5cbf704a897c04479acc241
Esteban <esteban.s.f0@gmail.com>
2015-11-17 19:49:14 +0100

    Weather App First Commit

---
+++ FreeCodeCamp/weather-app-angular/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherAppAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weatherAppAngularApp
+ */
+angular.module('weatherAppAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ FreeCodeCamp/weather-app-angular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherAppAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherAppAngularApp
+ */
+angular.module('weatherAppAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
